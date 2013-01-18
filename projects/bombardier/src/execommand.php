2c9d0d6b8581afe55d045ab487f752ca4f9782a4
Alexander Skoglund <alexander.skoglund@gmail.com>
2013-01-18 11:02:18 +0100

    Test function pointer.

--- projects/bombardier/src/execommand.php
+++ projects/bombardier/src/execommand.php
@@ -1,6 +1,15 @@
 <?php
-echo "Hello from execommand.php";
-echo "The command you whated to execute was:";
 
-echo $_GET['command']; 
+echo "Hello from execommand.php<p>";
+echo "The command you whated to execute was:<p>";
+echo $_GET['command'];
+echo "<p>";
+
+// you define this variable here so that it exists for the call to exec
+$output = null;
+
+// Windows users: 'dir c:\\' or something similar
+exec('func_pointer command', $output);
+echo "<pre>" . var_export($output, TRUE) . "</pre>";
+
 ?>
